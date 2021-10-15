75e7bf4708ed753a8ddbac971ec0728d1ba34aac
drdolgiy <alexey.dolgiy@gmail.com>
2021-10-16 02:27:37 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-const numberOfCategories = document.querySelectorAll('.item');
-console.log('Number of categories:', numberOfCategories.length) 
+const categoriesEl = document.querySelectorAll('.item');
+console.log('Number of categories:', categoriesEl.length);
 
-numberOfCategories.forEach(element => {
-console.log('Category:', element.firstElementChild.textContent)
-console.log('Element:', element.querySelectorAll('li').length)
-})
+categoriesEl.forEach(element => {
+    console.log('Category:', element.firstElementChild.textContent)
+    console.log('Element:', element.querySelectorAll('li').length)
+});
