11edc93b67e256564057329f94728e2187d321fe
drdolgiy <alexey.dolgiy@gmail.com>
2021-10-27 17:28:22 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const loginForm = document.querySelector(".login-form");
+console.log(loginForm);
 
+loginForm.addEventListener("submit", onLoginFormClick);
+
+function onLoginFormClick(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if ((email.value, password.value === "")) {
+    return alert("Bсе поля должны быть заполнены!");
+  }
+
+  const dataFormResult = {};
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, name) => {
+    dataFormResult[name] = value;
+  });
+
+  console.log(dataFormResult);
+
+  loginForm.reset();
+}
