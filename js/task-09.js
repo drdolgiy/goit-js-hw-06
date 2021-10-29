fa58d5ca1b9d845a06157315e8ac9aa6edf2e8e4
drdolgiy <alexey.dolgiy@gmail.com>
2021-10-30 00:16:47 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".widget");
+console.log(button);
+
+button.addEventListener("click", onChangeColorButtonClick);
+
+function onChangeColorButtonClick() {}
