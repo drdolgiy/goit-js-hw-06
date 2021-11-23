4a5d194f1968362a4e36c3959705dd1af59cf643
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2021-11-24 01:26:24 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,11 +2,16 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const button = document.querySelector(".change-color");
+const button = document.querySelector("button.change-color");
 console.log(button);
+
+const spanColor = document.querySelector("span.color")
+console.log(spanColor);
 
 button.addEventListener("click", onChangeColorButtonClick);
 
 function onChangeColorButtonClick(evt) {
-  
+  spanColor.textContent = getRandomHexColor().toString();
+
+  document.body.style.backgroundColor = getRandomHexColor()
 }
